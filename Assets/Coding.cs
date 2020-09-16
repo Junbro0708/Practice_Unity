acb672fea161659b58add8b3dd9a4b5b9dcd9fe5
Junbro0708 <qkrwns708@soongsil.ac.kr>
2020-09-16 15:28:41 +0900

    I studied unity

---
+++ Assets/Coding.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; // 네임 스페이스
+
+public class Coding : MonoBehaviour // 상위 클래스
+{
+    // Start is called before the first frame update
+    // 유니티가 호출해주는 두 개의 함수
+    void Start() // 만들어질 때 한 번 호출됨
+    {
+        string text;
+
+        text = "Hello World!";
+        print(text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
