c26c862a7f7d105c8739ee4cf8609ceb1715b1db
Junbro0708 <qkrwns708@soongsil.ac.kr>
2020-09-16 17:07:38 +0900

    I studied unity

--- Assets/Coding.cs
+++ Assets/Coding.cs
@@ -11,12 +11,37 @@ public class Coding : MonoBehaviour // 상위 클래스
         string text;
 
         text = "Hello World!";
-        print(text);
+        Debug.Log(text);
+
+        int number = 6;
+        if(number <= 0)
+        {
+            print("값이 0보다 작습니다.");
+        } else if(number >= 1 && number <= 5)
+        {
+            print("값이 1~5의 값입니다.");
+        } else
+        {
+            print("값이 5보다 큽니다.");
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() // 매 프레임 호출된다. 초당 60번 실행하면 계속 호출됨
     {
+        
+    }
+
+    void OnCollisionEnter(Collision theCollision)
+    {
+        if(theCollision.gameObject.name == "Floor")
+        {
+            Debug.Log("Hit the floor!");
+        } else if(theCollision.gameObject.name == "Cube1")
+        {
+            Debug.Log("Hit the Cube1!");
+            Destroy(GameObject.Find("Cube1"), 0);
+        }
         
     }
 }
---
+++ Assets/collisionApt.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionApt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "ground")
+        {
+            Debug.Log("Hit the floor!");
+        } else if(collision.gameObject.name != "ground")
+        {
+            Debug.Log("Hit another object!");
+            Destroy(collision.gameObject);
+        }
+    }
+}
