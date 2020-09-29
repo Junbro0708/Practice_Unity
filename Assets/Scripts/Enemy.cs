4261198484f299ee8279a0228341085310721f81
Junbro0708 <qkrwns708@soongsil.ac.kr>
2020-09-29 23:54:11 +0900

    I studied enemyAI with c#

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,7 +8,19 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Vector3 dir;
+
+        int randValue = UnityEngine.Random.Range(0, 10);
+
+        if(randValue < 3)
+        {
+            GameObject target = GameObject.Find("Player");
+            dir = target.transform.position - transform.position;
+            dir.Normalize();
+        }else
+        {
+            dir = Vector3.down;
+        }
     }
 
     private void OnCollisionEnter(Collision other)
