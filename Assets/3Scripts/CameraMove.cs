01e8db61973565b9ae9fb2c426cbbc0702265efa
Junbro0708 <qkrwns708@soongsil.ac.kr>
2020-11-18 19:04:36 +0900

    I studied camera moving using Unity with c#

---
+++ Assets/3Scripts/CameraMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public Transform centralAxis;
+    public Transform cam;
+    public float camSpeed;
+    float mouseX;
+    float mouseY;
+    float wheel;
+
+    void CamMove()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            mouseX += Input.GetAxis("Mouse X");
+            mouseY += Input.GetAxis("Mouse Y") * -1;
+
+            centralAxis.rotation = Quaternion.Euler(new Vector3(centralAxis.rotation.x + mouseY,
+                centralAxis.rotation.y + mouseX, 0) * camSpeed);
+        }
+    }
+    void Zoom()
+    {
+        wheel += Input.GetAxis("Mouse ScrollWheel");
+        /*if (wheel >= 1)
+            wheel = 1;
+        if (wheel <= -5)
+            wheel = -5;*/
+        cam.localPosition = new Vector3(0, 0, wheel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CamMove();
+        Zoom();
+    }
+}
---
+++ Assets/3Scripts/GameCharacterMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCharacterMove : MonoBehaviour
+{
+    public float speed = 0.5f;
+    SpriteRenderer spriteRenderer;
+    Animator anim;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        Vector3 dir = new Vector3(h, 0, 0);
+        transform.position += dir * speed * Time.deltaTime;
+
+        Debug.Log(h);
+
+        if (Input.GetButtonDown("Horizontal"))
+        spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+
+        if(h == 0)
+        {
+            anim.SetBool("isRun", false);
+        }else if(h != 0)
+        {
+            anim.SetBool("isRun", true);
+        }
+    }
+}
